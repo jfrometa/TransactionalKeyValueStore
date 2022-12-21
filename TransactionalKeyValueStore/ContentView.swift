891ad6ff8523c6f90953098a2d9c69667d727d23
Jose Frometa <jose.frometa@teapayment.co.uk>
2022-12-21 10:54:31 -0400

    initial commit

---
+++ TransactionalKeyValueStore/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TransactionalKeyValueStore
+//
+//  Created by Leo Salazar on 21/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TransactionalKeyValueStore/TransactionalKeyValueStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransactionalKeyValueStoreApp.swift
+//  TransactionalKeyValueStore
+//
+//  Created by Leo Salazar on 21/12/22.
+//
+
+import SwiftUI
+
+@main
+struct TransactionalKeyValueStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
